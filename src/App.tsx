ad3bf7f23f32518bbc42e423753f77688127060e
rifqiagniamubarok <rifqiagniamubarok@gmail.com>
2023-02-25 10:57:23 +0700

    udpatesssss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useRef, useState } from 'react';
+import Webcam from 'react-webcam';
+import QrScanner from 'qr-scanner';
 
 function App() {
+  const webcamRef = useRef<Webcam>(null);
+  const [fileImage, setFileImage] = useState<Blob>();
+
+  const videoConstraints = {
+    width: 300,
+    height: 300,
+    facingMode: { exact: 'environment' },
+    // facingMode: 'user',
+  };
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef?.current?.getScreenshot();
+    // console.log(imageSrc);
+    // let file = base64toFIle('sds', 'name');
+    // console.log(file);\
+    if (imageSrc !== null && imageSrc !== undefined) {
+      let arrs = imageSrc.split(',');
+      if (arrs) {
+        let val = arrs[0];
+        let mimecol = val.match(/:(.*?);/);
+        if (mimecol) {
+          let mime = mimecol[1];
+          let bstr = atob(arrs[1]);
+          let n = bstr.length;
+          let u8arr = new Uint8Array(n);
+          while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+          }
+          const file = new File([u8arr], 'name', { type: mime });
+          setFileImage(file);
+        }
+      }
+    }
+  }, [webcamRef]);
+
+  function base64toFIle(dataurl: string, filename: string) {
+    let arrs = dataurl.split(',');
+    if (arrs) {
+      let val = arrs[0];
+      let mimecol = val.match(/:(.*?);/);
+      if (mimecol) {
+        let mime = mimecol[1];
+        let bstr = atob(arrs[1]);
+        let n = bstr.length;
+        let u8arr = new Uint8Array(n);
+        while (n--) {
+          u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new File([u8arr], filename, { type: mime });
+      }
+    }
+  }
+
+  const readCodeImage = (file: Blob) => {
+    if (!file) {
+      return;
+    }
+    QrScanner.scanImage(file, { returnDetailedScanResult: true })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log('no data');
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <p>Coba scan</p>
+      <Webcam
+        audio={false}
+        // height={400}
+        // width={600}
+        ref={webcamRef}
+        videoConstraints={videoConstraints}
+        screenshotFormat="image/jpeg"
+      />
+      <button onClick={capture}>Capture</button>
+      {fileImage && <img src={URL.createObjectURL(fileImage)} alt="name" />}
+      {fileImage && (
+        <button onClick={() => readCodeImage(fileImage)}>Scan</button>
+      )}
     </div>
   );
 }
